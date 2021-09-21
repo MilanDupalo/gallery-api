12d778d5f82806fa37b00dab6ecbceec1e6e46fa
MilanDupalo <milandupalodupi@gmail.com>
2021-09-21 17:31:08 +0200

    seeder for gallery and users

---
+++ database/factories/GalleryFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Gallery;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class GalleryFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Gallery::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(3, true),
+            'description' => $this->faker->text(1000),
+            'user_id' => User::inRandomOrder()->first()->id,
+        ];
+    }
+}
